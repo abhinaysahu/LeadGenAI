21dfdbab878d4c7729a70d645880514b35ce5bee
abhinaysahu <abhin4ysahu@gmail.com>
2025-01-29 19:57:18 +0530

    Dashboard Updated

--- tamasha/src/components/RecentActivities.jsx
+++ tamasha/src/components/RecentActivities.jsx
@@ -1,19 +1,20 @@
 import callEllipse from "../assets/RecentActivities/CallEllipse.png";
-import callDoneIcon from "../assets/RecentActivities/CallDone.png";
+import callRinger from "../assets/RecentActivities/CallRinger.png";
+import convertedDone from "../assets/RecentActivities/ConvertedDone.png";
+import convertedEllipse from "../assets/RecentActivities/ConvertedEllipse.png";
+import followupEllipse from "../assets/RecentActivities/FollowUpEllipse.png";
+import followupClock from "../assets/RecentActivities/FollowUpClock.png";
 export default function RecentActivities() {
   return (
     <>
       <div className="text-2xl font-bold py-5 pl-10 border-b border-b-gray-400  text-gray-800">
         Recent Activities
       </div>
-      <div className="grid-cols-1 activities-container">
+      <div className="  grid-cols-1 activities-container">
         <div className=" py-3 activity-container border-b border-b-gray-400 items-center flex">
           <div className="activity-icon w-20 mx-10 relative">
             <img src={callEllipse}></img>
-            <img
-              className="absolute bottom-1.5 left-1"
-              src={callDoneIcon}
-            ></img>
+            <img className="absolute bottom-1 left-1" src={callRinger}></img>
           </div>
           <div className="activity-description flex flex-col w-5xl">
             <div className="heading text-xl font-semibold text-gray-700">
